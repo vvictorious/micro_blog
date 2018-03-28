8e59c16e9486cd318394a4f65b95064b851c10a6
vvictorious <vvictorious30@gmail.com>
2018-03-27 18:24:39 -0600

    just added buttons

---
+++ app.js
@@ -0,0 +1,22 @@
+									//goals
+
+//The homepage of your microblog app should be a list of these micro-posts with a form to add a new one. 
+
+// Your HTML page should have a list in it that will display the posts. It can be empty, with JS adding posts, or have some dummy posts in it to start.
+// Your JS will hold an array of posts that it appends to the HTML list.
+// When your user clicks submit, your JS should handle taking the content of the form and putting it in that array, and updating the DOM.
+// Make frequent commits and pushes. You should commit every time you add a new feature, and it is working! A feature can be something as small as adding a little style.
+
+									//don't freak out!!!!
+
+//	goal#1!!!!		make a button
+//	goal#2!!!!		make a list
+//	goal#3!!!!		make an array of posts that it appends to the HTML list
+//	goal#1!!!!		add posts using js
+
+
+let posts =  [
+"How to live a healthy life #inspo", "Benefits of green tea #woke", 
+"Be grateful #original",];
+
+console.log(posts);
